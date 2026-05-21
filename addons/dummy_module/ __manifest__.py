35083b291b55769947633381411f86a083cd359d
MolhamZL1 <molhamsa49@gmail.com>
2026-05-21 14:52:36 +0300

    ADD DMUMMY MODULE

---
+++ addons/dummy_module/ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Dummy Module',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Temporary placeholder module',
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
